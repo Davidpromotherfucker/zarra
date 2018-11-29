6a70fbe53e84c38d7794d3f909b78817b9cd0598
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-29 23:29:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("3help",{type: 'WATCHING'})
+   client.user.setActivity("3help",{type: 'LISTENING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
